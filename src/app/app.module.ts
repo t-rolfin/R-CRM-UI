87dd44cdaeb50f427acc33e558802738cd57a7fb
Florin Tatu <sk.florin@yahoo.com>
2021-05-27 22:17:42 +0300

    Added side bar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AuthLayoutRoutes } from './auth-layout.routing';
 import { AuthLayoutComponent } from './auth-layout.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    SidebarComponent
   ],
   imports: [
     RouterModule.forChild(AuthLayoutRoutes),
