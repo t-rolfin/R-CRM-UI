46da8cfac07dfcc851a4405f79bed1e8b7151ef4
Florin Tatu <rolfin.dc@gmail.com>
2021-05-30 00:11:15 +0300

    Added page for lead creation.

---
+++ src/app/pages/lead-creation/lead-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lead-creation',
+  templateUrl: './lead-creation.component.html',
+  styleUrls: ['./lead-creation.component.scss']
+})
+export class LeadCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
