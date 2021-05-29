71a9333136b96c6dd406b443f453ca5c019c25a7
Florin Tatu <rolfin.dc@gmail.com>
2021-05-29 23:41:55 +0300

    Added statistics page.

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router'
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { LeadsComponent } from '../../pages/leads/leads.component';
+import { StatisticsComponent } from '../../pages/statistics/statistics.component';
 
 export const AuthLayoutRoutes: Routes =
 [
   { path: '', component: DashboardComponent },
-  { path: 'leads', component: LeadsComponent }
+  { path: 'leads', component: LeadsComponent },
+  { path: 'statistics', component: StatisticsComponent }
 ]
