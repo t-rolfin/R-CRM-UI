7b25b80e9af51e8cc42679f49d409f5f1b647d92
Florin Tatu <rolfin.dc@gmail.com>
2021-05-29 23:27:41 +0300

    Added leads page.

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router'
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { LeadsComponent } from '../../pages/leads/leads.component';
 
 export const AuthLayoutRoutes: Routes =
 [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'leads', component: LeadsComponent }
 ]
